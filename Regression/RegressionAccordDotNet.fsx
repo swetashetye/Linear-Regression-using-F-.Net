e1f154d49247763a5b33e43b03b6a604acf0ef83
Sweta Shetye <sweta@akashic.in>
2017-12-05 16:35:37 +0530

    Add files via upload

---
+++ Regression/RegressionAccordDotNet.fsx
@@ -0,0 +1,72 @@
+﻿
+#r "System.Transactions.dll"
+
+
+#r "../packages/Accord.3.6.0/lib/net40/Accord.dll"
+#r "../packages/Accord.Statistics.3.6.0/lib/net40/Accord.Statistics.dll"
+#r "../packages/Accord.Math.3.6.0/lib/net40/Accord.Math.dll"
+#r "../packages/FSharp.Data.2.3.3/lib/net40/FSharp.Data.dll"
+
+
+
+open Accord
+open Accord.Statistics
+open Accord.Statistics.Models.Regression.Linear
+open System
+open System.Data.SqlClient
+
+
+type ProductReview = {ProductID:int; TotalOrders:float; AvgReviews:float}
+let reviews = ResizeArray<ProductReview>()
+[<Literal>]
+let connectionString = "data source=DESKTOP-QFG8KV0\SQLEXPRESS2014;initial catalog=AdventureWorks2014;user id=sa;password=Duffy@2945;"
+[<Literal>]
+let query = "Select
+            A.ProductID, TotalOrders, AvgReviews
+            From
+            (Select
+            ProductID,
+            Sum(OrderQty) as TotalOrders
+            from [Sales].[SalesOrderDetail] as SOD
+            inner join [Sales].[SalesOrderHeader] as SOH
+            on SOD.SalesOrderID = SOH.SalesOrderID
+            inner join [Sales].[Customer] as C
+            on SOH.CustomerID = C.CustomerID
+            Where C.StoreID is not null
+            Group By ProductID) as A
+            Inner Join
+            (Select
+            ProductID,
+            (Sum(Rating) + 0.0) / (Count(ProductID) + 0.0) as
+            AvgReviews
+            from [Production].[ProductReview] as PR
+            Group By ProductID) as B
+            on A.ProductID = B.ProductID"
+
+let connection = new SqlConnection(connectionString)
+let command = new SqlCommand(query,connection)
+connection.Open()
+let reader = command.ExecuteReader()
+while reader.Read() do
+    reviews.Add({ProductID=reader.GetInt32(0);TotalOrders=(float)(reader.GetInt32(1));AvgReviews=(float)(reader.GetDecimal(2))})
+
+
+let x = reviews |> Seq.map (fun pr -> pr.AvgReviews) |> Seq.toArray
+let y = reviews |> Seq.map (fun pr -> pr.TotalOrders) |> Seq.toArray
+
+let ols = OrdinaryLeastSquares()
+let regression =ols.Learn(x,y)
+let sse = regression.Regress(x,y)
+let mse = sse/float x.Length
+let rmse = sqrt mse
+let r2 = regression.CoefficientOfDetermination(x,y)
+
+
+
+
+
+//let regression = SimpleLinearRegression()
+//let sse = regression.Regress(x,y)
+//let mse = sse/float x.Length
+//let rmse = sqrt mse
+//let r2 = regression.CoefficientOfDetermination(x,y)
---
+++ Regression/Script.fsx
@@ -0,0 +1,47 @@
+﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#load "Library1.fs"
+open Regression
+
+// Define your library scripting code here
+
+let input = [|1,1.;2,2.;3,2.25;4,4.75;5,5.|]
+
+let variance (source:float seq) =  
+        let mean = Seq.average source
+        let deltas = Seq.map (fun x -> pown (x-mean) 2) source
+        Seq.average deltas
+
+let standardDeviation values =
+    sqrt (variance values)
+
+let x = input |> Seq.map (fun (x,y) -> float x)
+let y = input |> Seq.map (fun (x,y) -> y)
+let mX = Seq.average x
+let mY = Seq.average y
+let sX = standardDeviation x
+let sY = standardDeviation y
+
+
+// Pearsons Co relations
+
+let pearsonsCorrelation(a:float seq, b:float seq) =
+    let mX = Seq.average a
+    let mY = Seq.average b
+    let x = a |> Seq.map (fun x -> x - mX)
+    let y = b |> Seq.map (fun y -> y - mY)
+    let xys = Seq.zip x y
+    let xy = xys |> Seq.map (fun (x, y) -> x*y, x*x, y*y)
+    let sxy = xy |> Seq.sumBy (fun (xy, x2, y2) -> xy)
+    let sx2 = xy |> Seq.sumBy (fun (xy, x2, y2) -> x2)
+    let sy2 = xy |> Seq.sumBy (fun (xy, x2, y2) -> y2)
+    sxy / sqrt (sx2*sy2)
+
+
+Seq.sumBy (fun i -> i) [1;2;3]
+
+
+let r = pearsonsCorrelation (x,y)
+
+
